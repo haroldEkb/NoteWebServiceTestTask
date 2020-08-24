2963a768ca93663c3b3285cd1fe620953df5c85e
haroldekb <haroldekb@mail.ru>
2020-08-24 16:57:37 +0500

    Initial commit

---
+++ src/main/java/com/haroldekb/NoteWebServiceTestTask/NoteWebServiceTestTaskApplication.java
@@ -0,0 +1,15 @@
+package com.haroldekb.NoteWebServiceTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haroldekb@mail.ru
+ **/
+
+@SpringBootApplication
+public class NoteWebServiceTestTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoteWebServiceTestTaskApplication.class, args);
+    }
+}
